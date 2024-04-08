d4da5f4f37127c058e55e8cbf91445b420aeda45
André <andre.normann@hotmail.com>
2024-04-08 19:20:20 +0200

    mazemap

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <>
-    <h1>Map</h1>
-    </>
-  );
+    return (
+        <>
+            <h1>Map</h1>
+            <iframe
+                src="https://use.mazemap.com/#v=1&campusid=340&zlevel=3&center=5.331323,60.381346&zoom=19&sharepoitype=poi&sharepoi=1000724604"
+                style={{width: '75%', height: '600px'}}>
+            </iframe>
+        </>
+    );
 }
