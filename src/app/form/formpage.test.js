11e709591ae4ab94fd9c4faf3c0bf1580ff26408
jacobhaileyebio <@Jacob.Yebio>
2024-04-28 23:11:18 +0200

    added user test

--- src/app/form/formpage.test.js
+++ src/app/form/formpage.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import StudyRoomForm from './page';
-import * as nextRouter from 'next/navigation';
+import '@testing-library/jest-dom';
 
+// Mocking the useRouter hook from next/router
 jest.mock('next/navigation', () => ({
   useRouter() {
     return {
@@ -14,9 +15,29 @@ jest.mock('next/navigation', () => ({
   },
 }));
 
+// Mocking Firebase Auth and its dependencies
+jest.mock("firebase/auth", () => ({
+  getAuth: jest.fn().mockReturnValue({
+    currentUser: { uid: "testUser" } // Simulate that a user is logged in
+  }),
+  onAuthStateChanged: jest.fn((auth, callback) => {
+    callback({ uid: 'testUser' }); // Immediately invoke callback with user data
+    return jest.fn(); // Return a mock unsubscribe function
+  })
+}));
+
 describe('StudyRoomForm', () => {
   it('renders without crashing', () => {
     render(<StudyRoomForm />);
-    // Add any assertions here
+  });
+
+  it('renders the name input field', () => {
+    render(<StudyRoomForm />);
+    expect(screen.getByPlaceholderText('Ola Norman')).toBeInTheDocument();
+  });
+
+  it('renders the submit button', () => {
+    render(<StudyRoomForm />);
+    expect(screen.getByRole('button', { name: 'Submit' })).toBeInTheDocument();
   });
 });
