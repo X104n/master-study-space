4fc9d88e3d4eb34f6aa9b5858bbab0df8a368543
André <andre.normann@hotmail.com>
2024-04-28 22:23:12 +0200

    update

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: [
-        '@babel/preset-env', // Preset to compile ES2015+ syntax
-        '@babel/preset-react' // Preset for React to transform JSX into JavaScript
-    ],
-    plugins: [
-        '@babel/plugin-transform-runtime' // This plugin transforms the runtime environment
-    ]
-};
